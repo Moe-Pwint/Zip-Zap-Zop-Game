88ac9d0289cc4fef6d163052e9b812ed3d749e4b
Moe Pwint <moezatmoepwint@gmail.com>
2025-09-11 17:54:38 +0700

    Click correct card. Now, when the correct card is clicked, it sets the next card array, shuffle the card and save the next card array.

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,108 +4,66 @@ import './StartingUI.css'
 
 export default function Cards({ handleFalseCard, incrementScore }) {
   const [array, setArray] = useState([
-    { id: 'zip' },
-    { id: 'zap' },
-    { id: 'zop' },
+    { id: 'zip', correctCard: true },
+    { id: 'zap', correctCard: false },
+    { id: 'zop', correctCard: false },
   ])
 
   function handleShuffleArray(array) {
-    console.log(array)
     const arrCopy = [...array]
     shuffleArray(arrCopy)
-    console.log(arrCopy)
     setArray(arrCopy)
   }
 
-  // const [zipState, setZipState] = useState(true)
-  // const [zapState, setZapState] = useState(false)
-  // const [zopState, setZopState] = useState(false)
+  function checkFalseCard(e) {
+    if (e.target.classList.contains('false')) {
+      handleFalseCard()
+    } else {
+      incrementScore()
+      const nextArr = getNextArray(e.target)
+      handleShuffleArray(nextArr)
+    }
+  }
 
   return (
-    <div onClick={() => handleShuffleArray(array)} className="cardsContainer">
+    <div onClick={(e) => checkFalseCard(e)} className="cardsContainer">
       <CreatingCards array={array} />
     </div>
   )
 }
 
 function CreatingCards({ array }) {
-  // function handleZipClick() {
-  //   if (zipState === false) {
-  //     handleFalseCard()
-  //     return
-  //   }
-  //   console.log(zipState, zapState, zopState)
-  //   setZipState(false)
-  //   setZapState(true)
-  //   incrementScore()
-  // }
-
-  // function handleZapClick() {
-  //   if (zapState === false) {
-  //     handleFalseCard()
-  //     return
-  //   }
-  //   console.log(zipState, zapState, zopState)
-  //   setZapState(false)
-  //   setZopState(true)
-  //   incrementScore()
-  // }
-
-  // function handleZopClick() {
-  //   if (zopState === false) {
-  //     handleFalseCard()
-  //     return
-  //   }
-  //   console.log(zipState, zapState, zopState)
-  //   setZopState(false)
-  //   setZipState(true)
-  //   incrementScore()
-  // }
-  const arrCopy = array.slice()
-  const arr = arrCopy.map((i) => (
-    <button key={i.id} className={`card ${i}`}>
+  const arr = array.map((i) => (
+    <button key={i.id} className={`card ${i.id} ${i.correctCard}`}>
       <p>{i.id}</p>
     </button>
   ))
   return <>{arr}</>
 }
 
-// const zip = {id: 'zip',
-//   onClick: 'handleZipClick'
-// }
-
-// function Zip() {
-//   return (
-//     <button className="card">
-//       <p>ZIP</p>
-//     </button>
-//   )
-// }
-
-// function Zap() {
-//   return (
-//     <button className="card">
-//       <p>ZAP</p>
-//     </button>
-//   )
-// }
-
-// function Zop() {
-//   return (
-//     <button className="card">
-//       <p>ZOP</p>
-//     </button>
-//   )
-// }
-
-// function shuffleArray(array) {
-//   for (var i = array.length - 1; i > 0; i--) {
-//     var j = Math.floor(Math.random() * (i + 1))
-//     var temp = array[i]
-//     array[i] = array[j]
-//     array[j] = temp
-//   }
-// }
+function getNextArray(target) {
+  let nextArray = []
+  if (target.classList.contains('zip')) {
+    nextArray = [
+      { id: 'zip', correctCard: false },
+      { id: 'zap', correctCard: true },
+      { id: 'zop', correctCard: false },
+    ]
+  } else if (target.classList.contains('zap')) {
+    nextArray = [
+      { id: 'zip', correctCard: false },
+      { id: 'zap', correctCard: false },
+      { id: 'zop', correctCard: true },
+    ]
+  } else {
+    nextArray = [
+      { id: 'zip', correctCard: true },
+      { id: 'zap', correctCard: false },
+      { id: 'zop', correctCard: false },
+    ]
+  }
+  return nextArray
+}
 
 function shuffleArray(array) {
   for (var i = array.length - 1; i > 0; i--) {
