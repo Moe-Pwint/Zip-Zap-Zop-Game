839ad8945fb2554fc9e07f408add58b0dc63230d
Moe Pwint <moezatmoepwint@gmail.com>
2025-09-17 16:24:53 +0700

    Change timer from countdown to countup.

--- src/components/InfoTopDisplay.jsx
+++ src/components/InfoTopDisplay.jsx
@@ -57,12 +57,12 @@ const TimeUI = memo(function TimeUI({
   gamePlaying,
   gameWin,
 }) {
-  const [timer, setTimer] = useState(totalTime)
+  const [timer, setTimer] = useState(0)
 
   useEffect(() => {
-    if (gamePlaying && timer > 0) {
+    if (gamePlaying && timer < totalTime) {
       const interval = setInterval(() => {
-        setTimer(() => timer - 1)
+        setTimer(() => timer + 1)
       }, [1000])
 
       return () => {
@@ -73,13 +73,13 @@ const TimeUI = memo(function TimeUI({
 
   useEffect(() => {
     if (gamePlaying === false && gameWin) {
-      updateBestTime(totalTime - timer)
+      updateBestTime(timer)
     }
   })
 
   return (
     <div className="gameTimeContainer">
-      <p>Timer: {timer} seconds</p>
+      <p>Time Passed: {timer} seconds</p>
     </div>
   )
 })
@@ -100,95 +100,3 @@ const BestTimeUI = memo(function BestTimeUI({ bestTime }) {
     </div>
   )
 })
-
-// import React, { useEffect } from 'react'
-// import { useState, memo } from 'react'
-// import './StartingUI.css'
-
-// export default function InfoTopDisplay({
-//   totalTime,
-//   score,
-//   gamePlaying,
-//   gameWin,
-// }) {
-//   const savedBestTime = () => {
-//     if (localStorage.getItem('bestTime') === null) {
-//       localStorage.setItem('bestTime', totalTime)
-//       return localStorage.getItem('bestTime')
-//     } else {
-//       return localStorage.getItem('bestTime')
-//     }
-//   }
-//   const [bestTime, setBestTime] = useState(savedBestTime)
-//   const [timer, setTimer] = useState(totalTime)
-
-//   useEffect(() => {
-//     if (gamePlaying && timer > 0) {
-//       const interval = setInterval(() => {
-//         setTimer(() => timer - 1)
-//       }, [1000])
-
-//       return () => {
-//         clearInterval(interval)
-//       }
-//     }
-//   }, [gamePlaying, timer])
-
-//   useEffect(() => {
-//     if (gamePlaying === false && gameWin) {
-//       updateBestTime()
-//     }
-//   })
-
-//   function updateBestTime() {
-//     if (timer < bestTime) {
-//       console.log('yes')
-//       setBestTime(timer)
-//       localStorage.setItem('bestTime', timer)
-//     }
-//   }
-
-//   return (
-//     <>
-//       <div className="topContainer">
-//         <MenuUI />
-//         <TimeUI timer={timer} />
-//         <ScoreUI score={score} />
-//       </div>
-//       <BestTimeUI bestTime={bestTime} />
-//     </>
-//   )
-// }
-
-// const MenuUI = memo(function MenuUI() {
-//   return (
-//     <div className="menuContainer">
-//       <button className="menuBtn">Menu</button>
-//     </div>
-//   )
-// })
-
-// const TimeUI = memo(function TimeUI({ timer }) {
-//   return (
-//     <div className="gameTimeContainer">
-//       <p>Timer: {timer} seconds</p>
-//     </div>
-//   )
-// })
-
-// const ScoreUI = memo(function ScoreUI({ score }) {
-//   console.log('score updated')
-//   return (
-//     <div className="scoreContainer">
-//       <div>Score: {score}</div>
-//     </div>
-//   )
-// })
-
-// const BestTimeUI = memo(function BestTimeUI({ bestTime }) {
-//   return (
-//     <div className="bestTimeContainer">
-//       <p>Best Time: {bestTime} seconds </p>
-//     </div>
-//   )
-// })
